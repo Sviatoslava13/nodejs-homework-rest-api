be21daa77cc665149b6a09744150b5cc19dc3dfe
Sviatoslava13 <sviatoslavamar@gmail.com>
2023-01-19 15:16:59 +0200

    +++

--- routes/api/routerAuth.js
+++ routes/api/routerAuth.js
@@ -1,19 +1,22 @@
 const express = require("express");
+const multer = require("multer")
 const { authMiddlewares } = require("../../middlewares/authMiddlewares");
 const {
   signup,
   login,
   logout,
   getCurrent,
-  updateSubscription,
+    updateSubscription,
+  updateAvatar
 } = require("../../serviсes/users");
 
 const router = express.Router();
+const upload = multer({ dest: "tmp/" })
 
 router.post("/register", signup);
 router.get("/login", login);
 router.post("/logout", authMiddlewares, logout);
 router.get("/current", authMiddlewares, getCurrent);
 router.patch("/", updateSubscription);
-
+router.patch("/avatars", authMiddlewares, upload.single("avatar"), updateAvatar)
 module.exports = router;
